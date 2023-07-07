3e6a6026a043b69744b1715727411e0dea9a9e0e
Manuel Vega Uitz <mvegauitz@gmail.com>
2023-07-07 17:14:22 -0500

    Agregar archivos de proyecto.

---
+++ BillingPeriod/Controllers/BillingController.cs
@@ -0,0 +1,30 @@
+﻿using BillingPeriod.Helpers;
+using BillingPeriod.Models;
+using Microsoft.AspNetCore.Mvc;
+using static BillingPeriod.Helpers.PeriodRowCalculator;
+
+namespace BillingPeriod.Controllers
+{
+    public class BillingController : Controller
+    {
+        private readonly PeriodRowCalculator periodRowCalculator;
+
+        public BillingController()
+        {
+            periodRowCalculator = new DefaultPeriodRowCalculator();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateBillingCalendar(Period period)
+        {
+            List<PeriodRow> periodRows = periodRowCalculator.GeneratePeriodRows(period);
+
+            return RedirectToAction("Index", periodRows);
+        }
+    }
+}
---
+++ BillingPeriod/Models/Period.cs
@@ -0,0 +1,18 @@
+﻿namespace BillingPeriod.Models
+{
+    public class Period
+    {
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public int Periodicity { get; set; }
+        public int CuttingDay { get; set; }
+        public int PrintDay { get; set; }
+    }
+
+    public class PeriodRow
+    {
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public DateTime PrintDay { get; set; }
+    }
+}
